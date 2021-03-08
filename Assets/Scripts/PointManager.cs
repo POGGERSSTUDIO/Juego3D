78c55230a3d84bdb0cbc58b08174c05aae846a94
JordiItb <jordi.blanco.7e3@itb.cat>
2021-03-08 17:07:24 +0100

    FixedScripts
    Missing Scripts from merge fixed

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    public int score;
+    
+    public int getScore(){
+
+        return score;
+
+    }
+}
