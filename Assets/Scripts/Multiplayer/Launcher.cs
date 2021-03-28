624422f17b1d4b426506b4b1459d1898f73d7f5d
Cintia Canela Canadell <57130702+cynthiaaacc@users.noreply.github.com>
2021-03-28 20:24:39 +0200

    multijugador
    Co-Authored-By: JordiItb <72813365+JordiItb@users.noreply.github.com>
    Co-Authored-By: larios-gin <56580508+larios-gin@users.noreply.github.com>
    Co-Authored-By: paugarrido <56586203+paugarrido@users.noreply.github.com>

--- Assets/Scripts/Multiplayer/Launcher.cs
+++ Assets/Scripts/Multiplayer/Launcher.cs
@@ -18,6 +18,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] Transform playerListContent;
     [SerializeField] GameObject roomListItemPrefab;
     [SerializeField] GameObject PlayerListItemPrefab;
+    [SerializeField] GameObject startGameButton;
     
 
     void Awake()
@@ -35,6 +36,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Debug.Log("Connected to Master");
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
@@ -65,17 +67,34 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         Player[] players = PhotonNetwork.PlayerList;
 
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
         for (int i = 0; i < players.Count(); i++)
         {
             Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
         }
 
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+            
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         errorText.text = "Room Creation Failed: " + message;
         MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void StartGame() 
+    {
+        PhotonNetwork.LoadLevel(1);
     }
 
     
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();   
+    }
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PacmanController"), Vector3.zero, Quaternion.identity);
+    }
+}
